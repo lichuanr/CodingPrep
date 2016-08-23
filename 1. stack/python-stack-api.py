f5b56d48a4e07870ea7ffe0e1795079af061eccb
lichuanr <richard874410593@gmail.com>
2016-08-23 16:10:40 -0400

    linked list

--- 1. stack/python-stack-api.py
+++ 1. stack/python-stack-api.py
@@ -1,3 +1,4 @@
+#Using list to represent stack
 >>> stack = [3, 4, 5]
 >>> stack.append(6)
 >>> stack.append(7)
@@ -15,4 +16,30 @@
 [3, 4]
 
 
+#Using class to represent stack
+class Stack:
+     def __init__(self):
+         self.items = []
+
+     def isEmpty(self):
+         return self.items == []
+
+     def push(self, item):
+         self.items.append(item)
+
+     def pop(self):
+         return self.items.pop()
+
+     def peek(self):
+         return self.items[len(self.items)-1]
+
+     def size(self):
+         return len(self.items)
+        
+        
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+print stack.size()
 
